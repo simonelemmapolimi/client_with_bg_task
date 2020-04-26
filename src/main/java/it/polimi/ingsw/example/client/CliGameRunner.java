c63bc4b9395b4f434599ff99c852301f19b147f7
Simone <simone.lemma@mail.polimi.it>
2020-04-26 21:57:27 +0200

    godo come un porco

--- src/main/java/it/polimi/ingsw/example/client/CliGameRunner.java
+++ src/main/java/it/polimi/ingsw/example/client/CliGameRunner.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.example.client;
 
 public class CliGameRunner {
     /* sì ecco per eseguire il programma usa questo
+    dio
     * */
     public static void main(String args[]){
         UserInterfaceDue ui = new UserInterfaceDue();
